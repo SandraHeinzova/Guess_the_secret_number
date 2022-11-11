45c2fb7e7d60939424b64100c295dc77c418e6ad
Sandra Heinzová <heinzova.sandra@gmail.com>
2022-11-11 13:07:44 +0100

    Vytvořeny dvě def funkce podle obtížnosti

--- guessing_game.py
+++ guessing_game.py
@@ -9,22 +9,51 @@ hraju = "ano"
 print("Myslím si číslo od 1 do 100.")
 number = random.randint(0, 101)
 print (number)
-pokusy = 5
+# pokusy = 5
 
-while pokusy > 0:
-    print (f"Váš počet zbývajících pokusů je {pokusy}")
-    tip = int(input("Tipněte si číslo: "))
-    if tip > number:
-        print("Příliš vysoké!")
-        pokusy -= 1
-        if pokusy == 0:
-            print("Došly Vám pokusy! Prohráli jste. Mmožná příště.")
+def tezka_uroven():
+    pokusy = 5
+    while pokusy > 0:
+        print (f"Váš počet zbývajících pokusů je {pokusy}")
+        tip = int(input("Tipněte si číslo: "))
+        if tip > number:
+            print("Příliš vysoké!")
+            pokusy -= 1
+            if pokusy == 0:
+                print("Došly Vám pokusy! Prohráli jste. Mmožná příště.")
+                break
+        elif tip < number:
+            print("Příliš nízké!")
+            pokusy -= 1
+            if pokusy == 0:
+                print("Došly Vám pokusy! Prohráli jste. Mmožná příště.")
+                break
+        elif tip == number:
+            print("Vyhráli jste! Gratuluji!")
             break
-    elif tip < number:
-        print("Příliš nízké!")
-        pokusy -= 1
-        if pokusy == 0:
-            print("Došly Vám pokusy! Prohráli jste. Mmožná příště.")
+            
+def lehka_uroven():
+    pokusy = 10
+    while pokusy > 0:
+        print (f"Váš počet zbývajících pokusů je {pokusy}")
+        tip = int(input("Tipněte si číslo: "))
+        if tip > number:
+            print("Příliš vysoké!")
+            pokusy -= 1
+            if pokusy == 0:
+                print("Došly Vám pokusy! Prohráli jste. Mmožná příště.")
+                break
+        elif tip < number:
+            print("Příliš nízké!")
+            pokusy -= 1
+            if pokusy == 0:
+                print("Došly Vám pokusy! Prohráli jste. Mmožná příště.")
+                break
+        elif tip == number:
+            print("Vyhráli jste! Gratuluji!")
             break
-    elif tip == number:
-        print("Vyhráli jste! Gratuluji!")
+
+
+
+lehka_uroven()
+
