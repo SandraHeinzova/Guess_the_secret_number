d87d775a28368a54752754baa2ea595a457988c3
Sandra Heinzová <heinzova.sandra@gmail.com>
2022-11-11 08:51:55 +0100

    upravena while loop pro opakování hádání

--- guessing_game.py
+++ guessing_game.py
@@ -8,15 +8,17 @@ hraju = "ano"
 
 print("Myslím si číslo od 1 do 100.")
 number = random.randint(0, 101)
-pokusy = 5
 print (number)
-print (f"Váš počet zbývajících pokusů je {pokusy}")
+pokusy = 5
 
-tip = int(input("Tipněte si číslo: "))
-if tip > number:
-    print("Příliš vysoké. Zkuste to znovu.")
-elif tip < number:
-    print("Příliš nízké! Zkuste to znovu!")
-elif tip == number:
-    print("Vyhráli jste! Gratuluji!")
-    
+while pokusy > 0:
+    print (f"Váš počet zbývajících pokusů je {pokusy}")
+    tip = int(input("Tipněte si číslo: "))
+    if tip > number:
+        print("Příliš vysoké. Zkuste to znovu.")
+        pokusy -= 1
+    elif tip < number:
+        print("Příliš nízké! Zkuste to znovu!")
+        pokusy -= 1
+    elif tip == number:
+        print("Vyhráli jste! Gratuluji!")  
