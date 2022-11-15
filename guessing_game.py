c0e9e6bdee1656b2048eb5f16f7541e44b5b5b16
Sandra Heinzová <heinzova.sandra@gmail.com>
2022-11-15 21:30:02 +0100

    aesthetic modifications

--- guessing_game.py
+++ guessing_game.py
@@ -10,7 +10,7 @@ play_again = "yes"
 
 def guessing(attempts):
     print("Myslím si číslo od 0 do 100.")
-    secret_number = random.randint(0, 101)
+    secret_number = random.randint(0, 100)
     print(secret_number)
     # while loop to check valid difficulty input
     while True:
@@ -59,6 +59,7 @@ while play_again == "yes":
     play_again = input("Chcete hrát znovu? yes / no  "). lower()
     if play_again == "yes":
         print("Hra začíná znovu!")
+        os.system("cls")
     elif play_again != "no":
         print ("Tato odpověď nebyla v nabídce. Program bude ukončen.")
     
